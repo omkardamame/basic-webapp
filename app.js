bfa713563ed8b4eb5ec5a70ecc9ee9fc6bbcec70
omkardamame <omkardamame.work@gmail.com>
2025-04-25 23:35:19 +0530

    test1

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.get('/', (req, res) => {
     <body>
       <div class="container d-flex align-items-center justify-content-center vh-100">
         <div class="card shadow-lg p-4 w-100" style="max-width: 600px;">
-          <h2 class="mb-4 text-center">🌍 Your Visitor Info</h2>
+          <h2 class="mb-4 text-center">🌍🌍🌍🌍 Your Visitor Info</h2>
           <ul class="list-group mb-3">
             <li class="list-group-item"><strong>IP Address:</strong> ${ip}</li>
             <li class="list-group-item"><strong>City:</strong> ${city}</li>
